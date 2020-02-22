77c27f5ba1e79e8028355aa5e1976de9409158a3
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2020-02-21 22:03:42 -0800

    Imported Mongoose to connect to the MongoDB database.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyparser from 'body-parser';
 
 // initialize express
 const app = express();
 // define the port
 const PORT = 3000;
+
+// use Mongoose to connect to MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/MERN_app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // define routing for the root
 app.get('/', (req, res) =>
