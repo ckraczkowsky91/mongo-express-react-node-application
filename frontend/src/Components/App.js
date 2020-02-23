97483fe2056c08086deb8e15bbb2a1b9a7901e46
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2020-02-23 13:17:31 -0800

    Created three new React Components and added them to the App.js file.

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import PlayerList from './Player/PlayerList';
+import PlayerSingle from './Player/PlayerSingle';
+import PlayerForm from './Player/PlayerForm';
 
 function App() {
-  return (
-    <h1>Hello world!</h1>
+  return(
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col s12'>Menu</div>
+      </div>
+      <div className='row'>
+        <div className='col s3'><PlayerList /></div>
+        <div className='col s9'><PlayerSingle /></div>
+      </div>
+      <div className='row'>
+        <div className='col s12'><PlayerForm /></div>
+      </div>
+    </div>
   );
 };
 
